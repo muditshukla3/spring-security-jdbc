42658e3effc261f09c9a744cf55c93a1c4f51697
muditshukla3 <muditshukla3@gmail.com>
2020-07-26 10:22:43 +0530

    updated role for /admin

--- src/main/java/com/ms/com/ms/config/SecurityConfig.java
+++ src/main/java/com/ms/com/ms/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN","USER")
                 .antMatchers("/").permitAll().and().httpBasic();
     }
